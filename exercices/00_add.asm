5dcc3659dd2dc9041bb63bc5e4863e0b8d8b1b7d
Singh <taj.singh0715@gmail.com>
2020-03-25 00:38:24 +0100

    class one

--- exercices/00_add.asm
+++ exercices/00_add.asm
@@ -8,7 +8,16 @@
 # dans t0 à la fin du programme.
 
 mon_code: # <- Label
-li   t0, 32
-addi t0, t0, 0
-
+li   t0, 32 # load immediate: Charge un nombre sur max 12bits dans un registre
+addi t0, t0, 32 # Additionne operand1 et un immediat et stock le res dans destination
+#      |   |  
+#      |   operand1 		
+#      destination
+#
 # Bonus: avoir 64 dans un registre en une seule instruction
+
+li t1, 64
+li t2, 4096
+
+addi t2, zero, 32 
+add t3, t2, t2
--- exercices/01_syscall.asm
+++ exercices/01_syscall.asm
@@ -1,21 +1,27 @@
 # Exercice 1
 # utiliser un appel système comme write
 
-# On indique qu'on souhaite ranger des données dans le segment de data.
+# On indique qu'on souhaite ranger des données 
 .data
 
-# la directive .asciz permet de déclarer une chaine fini par un 0x00, '\0' en ascii.
-msg: .asciz "Hello ASM from .data"
+# .asciz permet de déclarer une chaine finie par un 0x00 en ascii
+msg: .asciz "Hello World" 
+#nombre: .word 32
 
 .text # <- segment de code.
 
 # Exercice: Observation dans Rars
+#
 # - Quelle est l'addresse de base de la chaine msg? Son addresse de fin?
 # - Dans quelle segment de la mémoire est elle?
 # - Quelle sont les valeurs en hexadecimales présente dans .data?
 
-la a0, ??? # argument de l'appel systeme
-li a7, ??? # appel systeme 4 selon la doc de Rars printString
-ecall # Peret de basculer en mode kernel pour faire l'appel systeme.
+#la: load Adress, charge une adresse (label) dans un registre. 
+la a0, msg
+li a7, 4 # appel systeme 4 selon la doc de Rars printString.
+#ecall # permet de basculer en mode kernel pour faire l'appel systeme
 
-# Bonus: Afficher maintenant un nombre, vous aurrez besoin de la documentation.
+# Bonus: avoir 64 dans un registre en une seule instruction
+li a0, 42
+li a7, 1
+ecall
